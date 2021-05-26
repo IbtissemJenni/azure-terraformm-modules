3dbff241a755a3c3575f8cf8dfc3d471818f3086
Cyberkabil10 <kabil.benhamda@gmail.com>
2021-05-27 00:47:50 +0100

    sensitive true

--- storage/output.tf
+++ storage/output.tf
@@ -1,8 +1,11 @@
 output "storage_access_key" {
+
   value = "${azurerm_storage_account.storage_account.primary_access_key}"
+  sensitive = true
 }
 
 output "storage_id" {
   value = "${azurerm_storage_account.storage_account.id}"
+  sensitive = true
 }
 
